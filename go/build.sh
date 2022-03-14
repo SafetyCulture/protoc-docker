f07347ba1b988d58beb0d6694780a00182a6be32
Roger Chapman <rogchap@gmail.com>
2022-03-14 11:44:16 +1100

    remove double install

--- go/build.sh
+++ go/build.sh
@@ -20,7 +20,6 @@ go install github.com/planetscale/vtprotobuf/cmd/protoc-gen-go-vtproto@latest
 # Includes HTTP Pattern fix that is in main branch but yet to be included in a release
 # Once included we can revert back to using @latest or better still @2.x
 go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@c0ecb42
-go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@latest
 
 go install github.com/SafetyCulture/s12-proto/protobuf/protoc-gen-govalidator@v$S12_PROTO_VERSION
 go install github.com/SafetyCulture/s12-proto/protobuf/protoc-gen-s12perm@v$S12_PROTO_VERSION
