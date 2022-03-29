cc09fa19a2536f3bbb37d483a61daa4af26158d8
Brandon Cook <brandon.cook@safetyculture.io>
2022-03-29 17:14:27 +1100

    Add build steps for other images

--- publish.sh
+++ publish.sh
@@ -3,19 +3,15 @@
 set -e
 
 REPOSITORY="ghcr.io/safetyculture"
-BASE_IMAGE="protoc"
 
-VERSION_FILE="./version.txt"
-IMAGE_NAME="$REPOSITORY/$BASE_IMAGE"
-if [ ! -z "$IMAGE" ]; then
-  VERSION_FILE="$IMAGE/version.txt"
-  IMAGE_NAME="$IMAGE_NAME-$IMAGE"
-fi
+VERSION_FILE="$IMAGE/version.txt"
 
 TAG=`cat $VERSION_FILE`
 if [ "$RELEASE_TAG" != "true" ]; then
 	TAG="$TAG-pre$(date +%Y%m%d%H%M%S)"
 fi
 
-echo "Building and pushing multi-arch docker container for $IMAGE_NAME:$TAG"
-docker buildx build --platform linux/amd64,linux/arm64 --push -t "$IMAGE_NAME":$TAG ./$IMAGE
+IMAGE_NAME="$REPOSITORY/$IMAGE:$TAG"
+
+echo "Building and pushing multi-arch docker container for $IMAGE_NAME"
+docker buildx build --platform linux/amd64,linux/arm64 --push -t "$IMAGE_NAME" ./$IMAGE
