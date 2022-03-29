5af27e49d56f591a96a8bac88e3df4a26dd4f45c
Brandon Cook <brandon.cook@safetyculture.io>
2022-03-29 17:19:34 +1100

    Fix spelling

--- protoc-go/build.sh
+++ protoc-go/build.sh
@@ -7,7 +7,7 @@ go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 go install github.com/planetscale/vtprotobuf/cmd/protoc-gen-go-vtproto@latest
 
 go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@latest
-go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@lastest
+go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@latest
 
 go install github.com/SafetyCulture/s12-proto/protobuf/protoc-gen-govalidator@v$S12_PROTO_VERSION
 go install github.com/SafetyCulture/s12-proto/protobuf/protoc-gen-s12perm@v$S12_PROTO_VERSION
