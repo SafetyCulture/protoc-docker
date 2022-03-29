21a66a4c04af0e409e0bee1e1b406252ca6c5147
Brandon Cook <brandon.cook@safetyculture.io>
2022-03-29 16:20:53 +1100

    [INTG-2227] Add build pipeline for `protoc` base image

---
+++ publish.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+REPOSITORY="ghcr.io/safetyculture"
+BASE_IMAGE="protoc"
+
+VERSION_FILE="./version.txt"
+IMAGE_NAME="$REPOSITORY/$BASE_IMAGE"
+if [ ! -z "$IMAGE" ]; then
+  VERSION_FILE="$IMAGE/version.txt"
+  IMAGE_NAME="$IMAGE_NAME-$IMAGE"
+fi
+
+TAG=`cat $VERSION_FILE`
+if [ "$RELEASE_TAG" != "true" ]; then
+	TAG="$TAG-pre$(date +%Y%m%d%H%M%S)"
+fi
+
+echo "Building and pushing multi-arch docker container for $IMAGE_NAME:$TAG"
+docker buildx build --platform linux/amd64,linux/arm64 --push -t "$IMAGE_NAME":$TAG ./$IMAGE
