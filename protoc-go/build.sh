432a5c9f351e1378a6818dd1ab944e5f91af751b
Mick Stanciu <mick.stanciu@safetyculture.io>
2024-09-16 13:24:00 +1000

    DEVP-3487 fixed version p1

--- protoc-go/build.sh
+++ protoc-go/build.sh
@@ -6,8 +6,8 @@ go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@v${
 go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@v${GRPC_GATEWAY_VERSION}
 go install github.com/planetscale/vtprotobuf/cmd/protoc-gen-go-vtproto@v0.6.0
 go install github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@v1.5.1
-go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.4.0
-go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.4.0
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.5.1
+go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.34.1
 
 go install github.com/SafetyCulture/protoc-gen-ratelimit/cmd/protoc-gen-ratelimit@latest
 go install github.com/SafetyCulture/protoc-gen-workato/cmd/protoc-gen-workato@latest
