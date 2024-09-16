20b3da01b9c25ac4b766c7c9e29cb341f65228a5
Mick Stanciu <mick.stanciu@safetyculture.io>
2024-09-16 15:26:56 +1000

    DEVP-3487 use latest protoc-go (#159)
    * DEVP-3487 use latest protoc-go
    
    * DEVP-3487 use latest protoc-go
    
    * DEVP-3487 rollback
    
    * DEVP-3487 rollback
    
    * DEVP-3487 rollback
    
    * DEVP-3487 rollback
    
    * DEVP-3487 fixed version p1
    
    * DEVP-3487 fixed version p1
    
    * DEVP-3487 fixed version p1
    
    * DEVP-3487 fixed version p1
    
    * DEVP-3487 fixed version p2
    
    * DEVP-3487 fixed version p3
    
    * DEVP-3487 fixed version p3

--- protoc-go/build.sh
+++ protoc-go/build.sh
@@ -2,12 +2,12 @@
 
 set -o errexit
 
-go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@latest
-go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@latest
-go install github.com/planetscale/vtprotobuf/cmd/protoc-gen-go-vtproto@latest
-go install github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@latest
-go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
-go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/protobuf/cmd/protoc-gen-go@v${PROTOC_GEN_GO_VERSION}
+go install github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@v${PROTOC_GEN_DOC_VERSION}
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v${PROTOC_GEN_GO_GRPC_VERSION}
+go install github.com/planetscale/vtprotobuf/cmd/protoc-gen-go-vtproto@v${PROTOC_GEN_GO_VTPROTO_VERSION}
+go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@v${PROTOC_GEN_GRPC_GATEWAY_VERSION}
+go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@v${PROTOC_GEN_GRPC_GATEWAY_VERSION}
 
 go install github.com/SafetyCulture/protoc-gen-ratelimit/cmd/protoc-gen-ratelimit@latest
 go install github.com/SafetyCulture/protoc-gen-workato/cmd/protoc-gen-workato@latest
