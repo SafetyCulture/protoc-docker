10ab00c59e88ad56f9030bdef2f431e14ab51014
Mick Stanciu <mick.stanciu@safetyculture.io>
2024-09-16 13:56:27 +1000

    DEVP-3487 fixed version p1

--- protoc-go/build.sh
+++ protoc-go/build.sh
@@ -4,10 +4,10 @@ set -o errexit
 
 go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@v${GRPC_GATEWAY_VERSION}
 go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@v${GRPC_GATEWAY_VERSION}
-go install github.com/planetscale/vtprotobuf/cmd/protoc-gen-go-vtproto@v0.6.0
-go install github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@v1.5.1
-go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.5.1
-go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.34.1
+go install github.com/planetscale/vtprotobuf/cmd/protoc-gen-go-vtproto@v${PSEUDOMUTO_PROTOC_GEN_DOC_VERSION}
+go install github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@v${PSEUDOMUTO_PROTOC_GEN_DOC_VERSION}
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v${GOOGLE_PROTOC_GEN_GO_GRPC_VERSION}
+go install google.golang.org/protobuf/cmd/protoc-gen-go@v${GOOGLE_PROTOBUF_VERSION}
 
 go install github.com/SafetyCulture/protoc-gen-ratelimit/cmd/protoc-gen-ratelimit@latest
 go install github.com/SafetyCulture/protoc-gen-workato/cmd/protoc-gen-workato@latest
